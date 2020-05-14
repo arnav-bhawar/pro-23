18a7a0f11b8e179ff57f121b16faf1d6683ffd93
arnav-bhawar <64008302+arnav-bhawar@users.noreply.github.com>
2020-05-14 22:33:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ function setup() {
   createSprite(400, 200, 50, 50);
 
   box1=new box(200,200,100,50);
+  box2= new box(100,150,10,60);
 
 }
 
@@ -13,6 +14,7 @@ function draw() {
   background(255,255,255);  
 
   box1.display();
+  box2.display();
 
 
 
