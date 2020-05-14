fca36145fc58a9673495038a3ba8b4b2e4212bfd
arnav-bhawar <64008302+arnav-bhawar@users.noreply.github.com>
2020-05-14 22:25:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var box1,box2,box3
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+  
+
+
+
+
+
+  drawSprites();
+}
